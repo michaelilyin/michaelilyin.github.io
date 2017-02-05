6ed3103e111a184b91bdb6c2f042fbfefe810bed
Michael Ilyin <michael.s.ilyin@gmail.com>
2017-02-05 22:16:27 +0300

    merge

---
+++ 2.chunk.js
@@ -0,0 +1,117 @@
+webpackJsonpac__name_([2],{
+
+/***/ 329:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = __webpack_require__(1);
+var blog_component_1 = __webpack_require__(334);
+var blog_routing_module_1 = __webpack_require__(337);
+var BlogModule = (function () {
+    function BlogModule() {
+    }
+    BlogModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                blog_routing_module_1.BlogRoutingModule
+            ],
+            declarations: [
+                blog_component_1.BlogComponent
+            ]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], BlogModule);
+    return BlogModule;
+}());
+exports.BlogModule = BlogModule;
+
+
+/***/ }),
+
+/***/ 334:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = __webpack_require__(1);
+var BlogComponent = (function () {
+    function BlogComponent() {
+    }
+    BlogComponent = __decorate([
+        core_1.Component({
+            template: __webpack_require__(342)
+        }), 
+        __metadata('design:paramtypes', [])
+    ], BlogComponent);
+    return BlogComponent;
+}());
+exports.BlogComponent = BlogComponent;
+
+
+/***/ }),
+
+/***/ 337:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var router_1 = __webpack_require__(131);
+var core_1 = __webpack_require__(1);
+var blog_component_1 = __webpack_require__(334);
+var routes = [
+    { path: '', component: blog_component_1.BlogComponent }
+];
+var BlogRoutingModule = (function () {
+    function BlogRoutingModule() {
+    }
+    BlogRoutingModule = __decorate([
+        core_1.NgModule({
+            imports: [router_1.RouterModule.forChild(routes)],
+            exports: [router_1.RouterModule]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], BlogRoutingModule);
+    return BlogRoutingModule;
+}());
+exports.BlogRoutingModule = BlogRoutingModule;
+
+
+/***/ }),
+
+/***/ 342:
+/***/ (function(module, exports) {
+
+module.exports = "<p>Blog</p>\r\n"
+
+/***/ })
+
+});
+//# sourceMappingURL=2.chunk.js.map
